ab1d32aeb30318cd1e593dc258841e00cc177c6e
Bruna Midori <100386080+Brumidori@users.noreply.github.com>
2023-02-10 08:43:32 -0300

    Delete target/classes directory

--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO pet (nome, especie, raca, sexo, peso, dono, tel_dono, ultima_vacina, ultimo_anti_pulga) VALUES
-('Yuki', 'gato', 'SRD', 'F', 4.2, 'Bruna', '11995842060', '2023-01-01', '2023-01-01');
-
-INSERT INTO pet (nome, especie, raca, sexo, peso, dono, tel_dono, ultima_vacina, ultimo_anti_pulga) VALUES
-('Haru', 'gato', 'SRD', 'F', 5.7, 'Bruna', '11995842060', '2023-01-01', '2023-01-01');
